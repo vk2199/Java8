5178ab59fa5c04af6e2842049745497f8552cfa3
Vivek kushwaha <vivekkushwahafeb25@gmail.com>
2026-01-16 16:20:35 +0530

    Employee name modifid

--- src/com/java8/Main.java
+++ src/com/java8/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<Employee> employeeList= Arrays.asList(
-                new Employee(1010,"Vivek Kushwaha","vivek@gmail.com","IT",56000),
+                new Employee(1010,"Aman Kushwaha","aman@gmail.com","IT",56000),
                 new Employee(1110,"Ramesh Kumar","Ramesh@gmail.com","HR",46000)
         );
 
